ea3dd7453f8a6e65045fb871ebd8dbb48f8d25b3
Joshua Hoban <joshua.hoban@countersight.co>
2023-06-16 14:16:56 +0200

    display weather metrics.

--- src/client/components/Weather_display.tsx
+++ src/client/components/Weather_display.tsx
@@ -1,8 +1,50 @@
 import React, { FC, useEffect, useState } from "react"
 import use_api from "../hooks/use_api"
+import {
+  Chart,
+  ChartSize,
+  DARK_THEME,
+  LayoutDirection,
+  Metric,
+  Settings,
+} from "@elastic/charts"
+import { EUI_CHARTS_THEME_DARK } from "@elastic/eui/dist/eui_charts_theme"
+import { EuiLoadingSpinner } from "@elastic/eui"
+
+interface Weather_report {
+  description: string
+  icon: string
+  id: number
+  main: string
+}
+/**
+ * A partial type of only relevant data returned from getting the current weather
+ */
+interface Current_Weather {
+  /**
+   * Temperature information
+   */
+  main: {
+    feels_like: number
+    humidity: number
+    temp: number
+    temp_max: number
+    temp_min: number
+  }
+  /**
+   * List of weather descriptions
+   */
+  weather: Weather_report[]
+  /**
+   * Cloud coverage
+   */
+  clouds: {
+    all: number
+  }
+}
 
 const Weather_display: FC = () => {
-  const [weather_report, set_weather_report] = useState({})
+  const [weather_report, set_weather_report] = useState<Current_Weather>()
   const { get } = use_api()
 
   const get_location_data: PositionCallback = (data) => {
@@ -12,11 +54,15 @@ const Weather_display: FC = () => {
 
   const get_current_weather = async (lat: number, lon: number) => {
     try {
-      const results = await get("weather", { params: { lat, lon } })
-      set_weather_report(results)
+      const results = await get<Current_Weather>("weather", { params: { lat, lon } })
+      set_weather_report({ ...results.data })
     } catch (error) {
       console.error(error)
     }
+  }
+
+  const on_position_error: PositionErrorCallback = (error) => {
+    alert(error)
   }
 
   useEffect(() => {
@@ -24,13 +70,54 @@ const Weather_display: FC = () => {
       console.error(`Your browser doesn't support Geolocation`)
     }
 
-    navigator.geolocation.getCurrentPosition(get_location_data)
+    navigator.geolocation.getCurrentPosition(get_location_data, on_position_error)
   }, [])
 
+  if (!weather_report)
+    return <EuiLoadingSpinner size="xxl" style={{ margin: "auto" }} />
+
+  const size: ChartSize = [600, 200]
   return (
     <>
-      <div>Weather_display</div>
-      <div>{JSON.stringify(weather_report)}</div>
+      {/*@ts-ignore*/}
+      <Chart size={size}>
+        <Settings baseTheme={DARK_THEME} theme={EUI_CHARTS_THEME_DARK.theme} />
+        <Metric
+          id="metricId"
+          data={[
+            [
+              {
+                color: "#F1D86F",
+                title: "Humidity",
+                value: weather_report?.main.humidity ?? 0,
+                domainMax: 100,
+                progressBarDirection: LayoutDirection.Vertical,
+                valueFormatter: (v) => `${v}%`,
+              },
+              {
+                color: "#F1D86F",
+                title: "Cloud Cover",
+                value: weather_report?.clouds.all ?? 0,
+                domainMax: 100,
+                progressBarDirection: LayoutDirection.Vertical,
+                valueFormatter: (v) => `${v}%`,
+              },
+              {
+                color: "#8CB2CD",
+                title: "Temperature",
+                extra: (
+                  <span>
+                    Feels like{" "}
+                    <strong>{`${weather_report.main.feels_like}`}&deg;</strong>
+                  </span>
+                ),
+                value: weather_report.main.temp,
+                valueFormatter: (v) => `${v.toFixed(2)}°`,
+              },
+            ],
+          ]}
+        />
+      </Chart>
     </>
   )
 }
--- src/server/controllers/weather.ts
+++ src/server/controllers/weather.ts
@@ -17,15 +17,22 @@ export const get_weather = async (
 
   try {
     debugLog("Getting current weather data")
-    const results = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`
-    )
+
+    const api_url = new URL("https://api.openweathermap.org/data/2.5/weather")
+    api_url.searchParams.append("lat", lat.toString())
+    api_url.searchParams.append("lon", lon.toString())
+    api_url.searchParams.append("appid", process.env.WEATHER_API_KEY ?? "")
+    api_url.searchParams.append("units", "metric")
+
+    debugLog(`Sending request to API: ${api_url.toString()}`)
+    //`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`
+    const results = await axios.get(api_url.toString())
 
     return res.json(results.data)
   } catch (error) {
     errorLog("Failed to get weather")
     errorLog(error)
-    
+
     if (axios.isAxiosError(error)) {
       if (error.status === 401) {
         return next(
